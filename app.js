c8b9f384326dfa22dfa61553a2068f09690d4af1
Nicolas Said <kal@lysanxia.(none)>
2011-11-12 14:36:23 -0500

    buhbuhbuh

--- app.js
+++ app.js
@@ -2,25 +2,44 @@
 /**
  * Module dependencies.
  */
-
 var express = require('express');
+var SessionMongoose = require("session-mongoose");
+
 var app = module.exports = express.createServer();
 
 /*
  * Database shit
 */
-mongoose = require('mongoose');
-db = mongoose.connect('mongodb://localhost/social7');
-require('./models/user.js');
+    //Connect to MongoDB
+    mongoose = require('mongoose');
+    app.db = mongoose.connect('mongodb://localhost/social7');
+    //Define schemas
+    require('./models/user.js');
+/*
+ *
+ */
 
+//
 // Configuration
+//
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.set('view engine', 'ejs');
+  var pub = __dirname + '/public';
+  app.use(express.compiler({ src: pub, enable: ['less'] }));
   app.use(express.bodyParser());
-  app.use(express.cookieParser());
   app.use(express.methodOverride());
+       
+  //Session mgmt
+  //Using mongoDB as a session store...
+  var mongooseSessionStore = new SessionMongoose({
+    url: "mongodb://localhost/session",
+    interval: 60000
+  });
+  app.use(express.cookieParser());
+  app.use(express.session({secret: "allyourbase", store: mongooseSessionStore}));
+  //
+  
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -33,32 +52,11 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
+//
 // Routes
-
-app.get('/', function(req, res){
-    var User = db.model('User'); 
-    var u = new User();
-    u.name = 'Fool';
-    u.email = 'nicolas.said@gmail.com';
-    u.save(function() {
-    User.find().all(function(arr) {
-    });
-    });
-
-  res.render('index', {
-    layout: true,
-    title: 'Social7',
-    user: u
-  });
-});
-
-app.get('/lol', function(req, res){
-    var User = db.model('User');
-    User.find( {}, function(err, usr){
-        res.render('lol', {usrarray: usr})
-    });
-    //res.send('polbak');
-});
+//
+require( './routes/site.js')(app);
+require( './routes/auth.js')(app);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- models/user.js
+++ models/user.js
@@ -1,6 +1,10 @@
-var mongoose = require('mongoose');
+//Requires
+var md5 = require('md5');
+
+//
 var Schema = mongoose.Schema;
 
+//
 var UserSchema = new Schema({
   email: {
     type: String,
@@ -10,7 +14,15 @@ var UserSchema = new Schema({
   lastseen: Date,
   isonline: Boolean,
   hashed_password: String,
-  salt: String
+  registered: Boolean
 });
 
-mongoose.model('User', UserSchema);
+UserSchema.virtual('password')
+    .get(function() {
+        return this.hashed_password;
+    })
+    .set(function(password) {
+        this.hashed_password = md5.digest_s(password);
+    });
+
+module.exports = mongoose.model('User', UserSchema);
